9aa320b160e71b0160f1337c5185590463d75a6f
MarceDiazG <marcelo.diaz@globant.com>
2017-08-31 21:05:09 -0300

    Basic functionality

---
+++ SpecFlowPages/Utils/ConstantsUtils.cs
@@ -0,0 +1,8 @@
+﻿namespace SpecFlowPages
+{
+    public class ConstantsUtils
+    {
+        public static string Url = "http://www.edgenuity.com";
+        public static string GetStartedExcelButtonText = "Get Started Now with SpecFlow+ Excel!";
+    }
+}
