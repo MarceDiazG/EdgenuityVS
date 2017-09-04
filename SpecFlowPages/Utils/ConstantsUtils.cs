56486d2dd33d1534d7f2838c297d0bc18538ae4c
MarceDiazG <marcelo.diaz@globant.com>
2017-09-04 18:04:59 -0300

    Add new mapping to Login Educator Portal

--- SpecFlowPages/Utils/ConstantsUtils.cs
+++ SpecFlowPages/Utils/ConstantsUtils.cs
@@ -2,7 +2,8 @@
 {
     public class ConstantsUtils
     {
-        public static string Url = "http://www.edgenuity.com";
+        public static string Url = "https://auth.qa.edgenuity.com/Login/Login/Educator";
+        public static string primaryUrl= "http://www.edgenuity.com";
         public static string GetStartedExcelButtonText = "Get Started Now with SpecFlow+ Excel!";
     }
 }
---
+++ SpecFlowTests/AcceptanceTests/LoginEducator.feature
@@ -0,0 +1,10 @@
+﻿Feature: Login Educator
+	In order to check funcionality
+	As a Educator user
+	I want to login into Educator Portal
+
+@Educator
+Scenario: Check login of Educator
+	Given Going to Educator Portal
+	When I complete credentials Marceloadmin and marcelo
+	Then The educator is succesfully logged
